9df233265acc640872e66850fa99b2a6bcd4c2e1
Sultana Yeasmin <yeasmins2005@gmail.com>
2024-01-09 10:21:42 -0500

    added fading motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [showAdditionalInfo, setShowAdditionalInfo] = useState(false);
+
+  useEffect(() => {
+    const fadeUpTimeout = setTimeout(() => {
+      setShowAdditionalInfo(true);
+    }, 3000);
+
+    return () => clearTimeout(fadeUpTimeout);
+  }, []);
+
+  useEffect(() => {
+    if (showAdditionalInfo) {
+      const additionalInfoTimeout = setTimeout(() => {
+        setShowAdditionalInfo(false);
+      }, 2000);
+
+      return () => clearTimeout(additionalInfoTimeout);
+    }
+  }, [showAdditionalInfo]);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1 id="heading" className={`animated fadeOutUp`} >
+          hello girlies
+        </h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      {showAdditionalInfo && (
+      <div>
+        <h1>How are you guys</h1>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
