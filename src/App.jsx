8d33bc6d747456445fc6df25e196c21a270915a3
Sultana Yeasmin <yeasmins2005@gmail.com>
2024-10-21 20:45:32 -0400

    added more projects & research

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import me from "./assets/me.jpg";
 import sudoku from "./assets/sudoku.gif";
 import crowd_surfer from "./assets/crowdsurfer.gif";
 import crossword from "./assets/crossword.gif";
+import tswiftbot from "./assets/tswiftbot.gif";
+import linlab from "./assets/linlab.jpg";
+import ddr from "./assets/ddr.gif";
 
 function App() {
   return (
@@ -25,23 +28,47 @@ function App() {
         <img src={me} className="pic" alt="Sultana Yeasmin" />
       </div>
 
+      <div className="intro">
+      <img src={linlab} className="linlab" alt="Lin Lab Logo" />
+        <div>
+          <h1>Research</h1>
+          <p className="right-align">Under the guidance of Dr. Baihan Lin, developed a saliency estimation model with configuration options 
+          for visual, auditory, and novel semantic input, whereas previous models were solely audio-visual based. 
+          Incorporated semantic information as BERT sentence embeddings and extracted features using ResNets. 
+          Redesigned the fusion module to support multimodal input combinations (audio-visual, audio-semantic, 
+          visual-semantic, and audio-visual-semantic) using Bilinear layers and Linear transformations for 
+          comprehensive saliency prediction.</p>
+        </div>
+      </div>
+
       <div className="row">
-        <h1>Projects & Research</h1>
+        <h1>Projects</h1>
         <div className="column">
-          <a href="https://replit.com/@Sultana2/soduku-repl" target="_blank">
-            <img src={sudoku} className="projectgif" alt="Sudoku Project" />
+          <a href="https://github.com/saloni-jain-code/AI-Support-Bot" target="_blank">
+            <img src={tswiftbot} className="projectgif" alt="Taylor Swift Chatbot" />
           </a>
-          <p>Command Line Sudoku</p>
+          <p>Taylor Swift ChatBot</p>
+        </div>
+        <div className="column">
+          <a href="https://github.com/Sultanax/Dance-Dance-Revolution-Arduino-Game.git" target="_blank">
+            <img src={ddr} className="projectgif" alt="Dance Dance Revolution Game" />
+          </a>
+          <p>Dance Dance Revolution Arcade Game</p>
         </div>
         <div className="column">
           <a href="https://devpost.com/software/crowd-surfer" target="_blank">
             <img src={crowd_surfer} className="projectgif" alt="Crowd Surfers App" />
           </a>
-          <p>Crowd Surfers App:</p>
-          <p>View Library and Dining Hall Crowds!</p>
+          <p>Crowd Surfers</p>
         </div>
         <div className="column">
-          <a href="https://replit.com/@Sultana2/Crossword-puzzle" target="_blank">
+          <a href="https://github.com/Sultanax/Sudoku" target="_blank">
+            <img src={sudoku} className="projectgif" alt="Sudoku Project"></img>
+          </a>
+          <p>Command Line Sudoku</p>
+        </div>
+        <div className="column">
+          <a href="https://github.com/Sultanax/Crossword-Puzzle" target="_blank">
             <img src={crossword} className="projectgif" alt="Crossword Puzzle" />
           </a>
           <p>Command Line Crossword Puzzle</p>
